6047eb2257f52a45316abef51f49a45495541f0c
Auron <auronwalkeris@gmail.com>
2024-10-07 13:13:26 +1300

    changed led to send tuples

--- software/lib/basic.py
+++ software/lib/basic.py
@@ -13,16 +13,12 @@ class Basic:
     # takes a string of . and # the size of the LED matrix turn LED on for every # and off for every.
     def show_leds(self, plot):
         rows = grid_str.splitlines()
-        row_binarys = []
-
-        for row in rows:
-            binary = int(''.join('1' if char == '#' else '0' for char in row))
-            row_binarys.append(row_binary)
+        row_binarys = tuple(int(''.join('1' if char == '#' else '0' for char in row), 2) for row in rows) #makes a tuple with each row being stored in binary
 
         led.update_buffer(row_binarys)
     
     def clear_screen(self):
-        led.update_buffer([00000, 00000, 00000, 00000, 00000])
+        led.clear()
     
     def show_arrow(self, direction):
         #takes a direction and displays a arrow in that direction on LEDs
